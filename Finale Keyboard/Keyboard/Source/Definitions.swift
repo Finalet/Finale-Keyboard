89fef1316e1ff89dab72413e7994397ea3731066
Grant Oganyan <grant1599@mail.ru>
2026-04-11 23:16:32 -0400

    Update Definitions.swift

--- Finale Keyboard/Keyboard/Source/Definitions.swift
+++ Finale Keyboard/Keyboard/Source/Definitions.swift
@@ -26,16 +26,16 @@ enum Function {
     
     var icon: UIImage? {
         switch self {
-        case .Shift: return UIImage(systemName: "arrow.up", withConfiguration: UIImage.SymbolConfiguration(weight: .black))
-        case .SymbolsShift: return UIImage(systemName: "number", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
-        case .ExtraSymbolsShift: return UIImage(systemName: "numbers", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
-        case .Caps: return UIImage(systemName: "arrow.up.to.line", withConfiguration: UIImage.SymbolConfiguration(weight: .black))
-        case .Backspace: return UIImage(systemName: "delete.left.fill")
-        case .Back: return UIImage(systemName: "arrow.uturn.left", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
-        case .SymbolsToggle: return UIImage(systemName: "numbers", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
-        case .SymbolsToggleBack: return UIImage(systemName: "characters.uppercase", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
-        case .EmojiToggle: return UIImage(systemName: "face.smiling", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
-        case .Return: return UIImage(systemName: "return", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
+            case .Shift: return UIImage(systemName: "arrow.up", withConfiguration: UIImage.SymbolConfiguration(weight: .black))
+            case .SymbolsShift: return UIImage(systemName: "number", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
+            case .ExtraSymbolsShift: return UIImage(systemName: "numbers", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
+            case .Caps: return UIImage(systemName: "arrow.up.to.line", withConfiguration: UIImage.SymbolConfiguration(weight: .black))
+            case .Backspace: return UIImage(systemName: "delete.left.fill")
+            case .Back: return UIImage(systemName: "arrow.uturn.left", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
+            case .SymbolsToggle: return UIImage(systemName: "numbers", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
+            case .SymbolsToggleBack: return UIImage(systemName: "characters.uppercase", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
+            case .EmojiToggle: return UIImage(systemName: "face.smiling", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
+            case .Return: return UIImage(systemName: "return", withConfiguration: UIImage.SymbolConfiguration(weight: .semibold))
         }
     }
     
@@ -49,33 +49,33 @@ enum Function {
 
     func TapAction () {
         switch self {
-        case .Shift: FinaleKeyboard.instance.ShiftAction()
-        case .SymbolsShift: FinaleKeyboard.instance.ToggleExtraSymbolsView()
-        case .ExtraSymbolsShift: FinaleKeyboard.instance.ToggleExtraSymbolsView()
-        case .Caps: FinaleKeyboard.instance.ShiftAction()
-        case .Backspace: FinaleKeyboard.instance.BackspaceAction()
-        case .Back: FinaleKeyboard.instance.BackAction()
-        case .EmojiToggle: FinaleKeyboard.instance.OpenEmoji()
-        case .Return: FinaleKeyboard.instance.ReturnAction()
-        default: return
+            case .Shift: FinaleKeyboard.instance.ShiftAction()
+            case .SymbolsShift: FinaleKeyboard.instance.ToggleExtraSymbolsView()
+            case .ExtraSymbolsShift: FinaleKeyboard.instance.ToggleExtraSymbolsView()
+            case .Caps: FinaleKeyboard.instance.ShiftAction()
+            case .Backspace: FinaleKeyboard.instance.BackspaceAction()
+            case .Back: FinaleKeyboard.instance.BackAction()
+            case .EmojiToggle: FinaleKeyboard.instance.OpenEmoji()
+            case .Return: FinaleKeyboard.instance.ReturnAction()
+            default: return
         }
     }
     
     func LongPressAction () {
         switch self {
-        case .Shift: FinaleKeyboard.instance.ToggleAutoCorrect()
-        case .Caps: FinaleKeyboard.instance.ToggleAutoCorrect()
-        case .Backspace: FinaleKeyboard.instance.ShowShortcutPreviews()
-        default: return
+            case .Shift: FinaleKeyboard.instance.ToggleAutoCorrect()
+            case .Caps: FinaleKeyboard.instance.ToggleAutoCorrect()
+            case .Backspace: FinaleKeyboard.instance.ShowShortcutPreviews()
+            default: return
         }
     }
     
     func LongPressEndedAction () {
         switch self {
-        case .Backspace: FinaleKeyboard.instance.HideShortcutPreviews()
-        case .SymbolsToggle: FinaleKeyboard.instance.ToggleSymbolsView()
-        case .SymbolsToggleBack: FinaleKeyboard.instance.ToggleSymbolsView()
-        default: return
+            case .Backspace: FinaleKeyboard.instance.HideShortcutPreviews()
+            case .SymbolsToggle: FinaleKeyboard.instance.ToggleSymbolsView()
+            case .SymbolsToggleBack: FinaleKeyboard.instance.ToggleSymbolsView()
+            default: return
         }
     }
     
